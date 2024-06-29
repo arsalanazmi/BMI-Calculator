70e79203962de003e437f4d8b5a546be43e769d0
Arsalan Raza Azmi <arsalan.azmi92@gmail.com>
2024-06-29 13:00:04 +0500

    condition added for invalid height & weight

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("#form");
-// const reset = document.querySelector("#reset");
-
+const reset = document.querySelector("#reset");
+const heightError = document.querySelector(".heightError");
+const weightError = document.querySelector(".weightError");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -8,12 +9,20 @@ form.addEventListener("submit", (e) => {
   const weight = document.querySelector(".weight").value;
   const result = document.querySelector(".result");
 
-  const bmi = ((weight / height ** 2) * 10000).toFixed(2);
-  result.value = bmi;
+  if (height <= 0 || height == "") {
+    heightError.setAttribute("style", "display:block !important");
+    heightError.innerHTML = "Please enter valid height in cm.";
+  }
+  if (weight <= 0 || weight == "") {
+    weightError.setAttribute("style", "display:block !important");
+    weightError.innerHTML = "Please enter valid weight in kg.";
+  } else {
+    const bmi = ((weight / height ** 2) * 10000).toFixed(2);
+    result.value = bmi;
+  }
 });
 
-// form.addEventListener("reset", (e) => {
-//   const height = (document.querySelector(".height").value = null);
-//   const weight = (document.querySelector(".weight").value = null);
-//   const result = (document.querySelector(".result").value = null);
-// });
+form.addEventListener("reset", (e) => {
+  heightError.innerHTML = "";
+  weightError.innerHTML = "";
+});
